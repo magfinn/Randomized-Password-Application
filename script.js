94b50c883f97a9e3ed8aa8ed068c23cf9daf82a7
wisner.maggiel@gmail.com <wisners@Wisnerss-iMac.local>
2021-10-17 15:12:33 -0700

    added array of all possible outcomes

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z' ];
+var confirmNumbers = [0,1,2,3,4,5,6,7,8,9];
+var confirmCharacters = [".","!",":","@","#","$","%","^","&","*","(",")","_","+","-","="];
 
 //when the user clicks on generate password
-
 
 //create eventListener for onclick generate password button
 
@@ -14,16 +18,18 @@ var promptLength = window.prompt ("How long do you want your password to be (cho
 console.log (promptLength);
 
 //if user clicks "cancel" verify that they want to cancel (how do I include this throughout?)
-if ("Cancel"){
-  var confirmCancel = 
-  window.confirm ("Are you sure you would like to cancel your password generation?");
-}
+
+//if ("Cancel"){
+  //var confirmCancel = 
+  //window.confirm ("Are you sure you would like to cancel your password generation?");
+//}
+
 //if yes, then cancel
-if(confirmCancel) {
-  window.alert("See you next time!");
-  console.log ("cancelled");
+//if(confirmCancel) {
+  //window.alert("See you next time!");
+  //console.log ("cancelled");
   //how do you end the submission?
-}
+//}
 
 //if user selects number less than 9 or greater than 128, they get an error notice
 if (promptLength >128 || promptLength < 9) {
@@ -31,36 +37,28 @@ if (promptLength >128 || promptLength < 9) {
   var promptLength = window.prompt ("How long do you want your password to be (choose a number between 9-128)");
 }
 
+//input upperCase letters
+var promptUppercase = window.prompt ("Should we include uppercase letters?")
+
+if (promptUppercase ==="yes" || promptUppercase === "Yes") {
+  console.log (upperCase);
+}
+
 //input numbers
-var promptNumbers = window.prompt ("Would you like to include numbers in your password?");
+var promptNumbers = window.prompt ("Would you like to include numbers in your password? (please type 'yes' or 'no')");
 
-console.log (promptNumbers);
-
-//if user selects "yes" then exclude numbers from password
-if(promptNumbers = "yes" || promptNumbers = "Yes");
-console.log ("confirmNumbers");
-
-//if user selects "no" then include numbers in password - maybe I don't need to create a false statement?
-
+if (promptNumbers === "yes" ||  promptNumbers === "Yes") {
+  console.log (confirmNumbers);
+}
 
 //input special characters
-var promptCharacters = window.prompt ("Would you like to include special characters '&!#%' in your password?");
+var promptCharacters = window.prompt ("How about special characters (yes or no)");
 
-console.log (promptCharacters);
+if(promptNumbers === "yes" || promptNumbers === "Yes") {
+console.log (confirmCharacters);
+}
 
-//if user selects "yes" then include special characters from password
-if(promptNumbers = "yes" || promptNumbers = "Yes");
-console.log ("confirmCharacters");
-
-//if user selects "no" then exclude special characters from password - maybe I don't need to include a false answer?
-
-
-
-
-
-
-
-
+console.log ("all possible outputs" + confirmCharacters + confirmNumbers + upperCase + alphabet)
 
 
 // Write password to the #password input
@@ -69,20 +67,27 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var password = ("Your password is")
-//how to concat a random password)
+//All possible options//
+function writePassword() {
+  (upperCase + alphabet + confirmCharacters + confirmNumbers);
+} ;
+var password = ("Your password is + writePassword")
 
-
+//write all possible outcomes - haven't figured out how to pick random ones yet
+writePassword ()
 
 //display password
 document.write=password
 
 
+
+
+//use a for loop to create all possible options//
+
+
+  
